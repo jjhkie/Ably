adedfcd7ac08d7a1018239f5231f31dbc09bf5ee
jjhkie <83710866+jjhkie@users.noreply.github.com>
2023-01-09 20:35:50 +0900

    [MyPage] Button 속성 수정 완료

--- Ably/App/Scene/Mypage/Utils/TableHeader.swift
+++ Ably/App/Scene/Mypage/Utils/TableHeader.swift
@@ -15,9 +15,7 @@ final class TableHeader: UITableViewCell{
     
     private let bag = DisposeBag()
     
-    private let allStack = UIStackView()
     private let textStack = UIStackView()
-    private let buttonStack = UIStackView()
     
     private let grayText = UILabel().then{
         $0.text = "에이블리 회원이라면"
@@ -30,11 +28,15 @@ final class TableHeader: UITableViewCell{
         $0.textColor = .black
     }
     
+    
+    
     private let loginButton = UIButton().then{
         $0.setTitle("로그인", for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 14, weight: .bold)
     }
     private let signupButton = UIButton().then{
         $0.setTitle("회원가입", for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 14, weight: .bold)
     }
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -46,8 +48,6 @@ final class TableHeader: UITableViewCell{
         
        
     }
-    
-
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -75,42 +75,55 @@ extension TableHeader{
     
     
     private func attribute(){
-        //스택뷰 설정
-        [allStack,buttonStack].forEach{
-            $0.axis = .horizontal
-            $0.distribution = .fillEqually
-        }
+
         textStack.axis = .vertical
-        textStack.spacing = 5
+        textStack.spacing = 2
         
         [loginButton,signupButton].forEach{
+            //$0.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
             $0.setTitleColor(UIColor.gray, for: .normal)
-            $0.layer.cornerRadius = 20
+            $0.layer.cornerRadius = 30
             $0.layer.borderWidth = 1
-            $0.layer.borderColor = UIColor.gray.cgColor
+            $0.layer.borderColor = UIColor.gray.withAlphaComponent(0.5).cgColor
         }
         
     }
     
     private func layout(){
         [grayText,blackText].forEach{
+            
             textStack.addArrangedSubview($0)
         }
         
         [loginButton,signupButton].forEach{
-            buttonStack.addArrangedSubview($0)
+            $0.sizeToFit()
+            $0.translatesAutoresizingMaskIntoConstraints = false
+
         }
         
-        [textStack,buttonStack].forEach{
-            allStack.addArrangedSubview($0)
+        
+        
+
+        [textStack, loginButton,signupButton].forEach{
+            contentView.addSubview($0)
         }
-        
-        contentView.addSubview(allStack)
-        
-        allStack.snp.makeConstraints{
+
+        textStack.snp.makeConstraints{
             $0.top.equalTo(contentView.safeAreaLayoutGuide).inset(20)
-            $0.trailing.leading.bottom.equalToSuperview().inset(20)
-            //$0.edges.equalToSuperview()
+            $0.leading.bottom.equalToSuperview().inset(20)
+        }
+        loginButton.snp.makeConstraints{
+            $0.width.equalTo(70)
+            $0.top.equalTo(textStack.snp.top)
+            $0.leading.equalTo(textStack.snp.trailing)
+            $0.centerY.equalToSuperview()
+        }
+        signupButton.snp.makeConstraints{
+            $0.width.equalTo(80)
+            $0.top.equalTo(loginButton.snp.top)
+            $0.leading.equalTo(loginButton.snp.trailing).offset(5)
+            $0.trailing.equalToSuperview().inset(20)
+            $0.centerY.equalToSuperview()
         }
     }
 }
